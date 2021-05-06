8e77ec8dffee309ac5c7949644e6de0d7ea24bcf
Fernancode548 <fmontesinosestrada@gmail.com>
2021-05-05 22:11:40 -0500

    Primera versión de StoreZapatillas

---
+++ js/style.js
@@ -0,0 +1,10 @@
+const typed = new Typed('.typed', {
+    strings: ['zapatillas','trainers','shoes','sneakers'],
+
+    typeSpeed: 78,
+    startDelay: 1000,
+    backSpeed: 75,
+    backDelay: 2500,
+    loop: true,
+    showCursor: true,
+})
